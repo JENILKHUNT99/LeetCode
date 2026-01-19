1b38d62b04af5b550b23eb006c9bd5a30302de89
JENILKHUNT99 <jenilkhunt22222@gmail.com>
2026-01-19 21:45:27 +0530

    LeetCode 178

---
+++ 178.py
@@ -0,0 +1,16 @@
+#MySql:
+"""
+select 
+    score , 
+    dense_rank() over (order by score desc) as `rank` 
+from Scores 
+"""
+#Python:(Pandas)
+'''
+import pandas as pd
+
+def order_scores(scores: pd.DataFrame) -> pd.DataFrame:
+    scores['rank']=scores['score'].rank(method="dense",ascending=False)
+    scores=scores.drop('id',axis=1).sort_values(by="score",ascending=False)
+    return scores
+'''
