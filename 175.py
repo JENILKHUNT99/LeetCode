71e2e9e585d074eb12ff5c184ff8e4a39d176285
JENILKHUNT99 <jenilkhunt22222@gmail.com>
2026-01-19 12:24:43 +0530

    Changes

--- 175.py
+++ 175.py
@@ -9,9 +9,11 @@ FROM Person p
 LEFT JOIN Address a ON p.personId = a.personId;
 """
 #Python:(Pandas)
+'''
 import pandas as pd
 
 def combine_two_tables(person: pd.DataFrame, address: pd.DataFrame) -> pd.DataFrame:
     result = person.merge(address,on="personId",how="left")
     result=result[['firstName','lastName','city','state']]
     return result
+'''
