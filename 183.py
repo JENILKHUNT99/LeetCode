0f56aee9ebf972e786dccdb9de7aeaaaf059c6b8
JENILKHUNT99 <jenilkhunt22222@gmail.com>
2026-01-21 22:42:35 +0530

    LeetCode 183

--- 183.py
+++ 183.py
@@ -5,3 +5,11 @@ FROM Customers C
 LEFT JOIN Orders O 
 ON C.id=O.customerId WHERE O.id is null
 '''
+#Python:(Pandas)
+'''
+import pandas as pd
+
+def find_customers(customers: pd.DataFrame, orders: pd.DataFrame) -> pd.DataFrame:
+    Ans=customers[~customers['id'].isin(orders['customerId'])]
+    return pd.DataFrame({'Customers':Ans['name']})
+'''
