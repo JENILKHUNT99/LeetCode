5f15a30b2da2df8d16aee684d0e07a06ab3d9244
JENILKHUNT99 <jenilkhunt22222@gmail.com>
2026-01-22 14:15:18 +0530

    LeetCode 9

---
+++ 7.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def reverse(self, x):
+        """
+        :type x: int
+        :rtype: int
+        """
+        sing = -1 if x<0 else 1
+        absv = abs(x)
+        rev = int(str(absv)[::-1])
+        
+        if rev>2**31 or rev<-2**31:
+            return 0
+
+        return sing * rev
