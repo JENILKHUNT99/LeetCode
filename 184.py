4aba5c2c8a2e172a8901b9de22ccfa4d1256b3aa
JENILKHUNT99 <jenilkhunt22222@gmail.com>
2026-01-27 21:15:20 +0530

    LeetCode 184

---
+++ 184.py
@@ -0,0 +1,16 @@
+#MySql:
+'''
+SELECT
+    D.name AS Department,
+    E.name AS Employee,
+    E.salary AS Salary
+FROM Employee AS E
+INNER JOIN Department AS D ON E.departmentId = D.id
+WHERE (E.departmentId,E.salary) IN 
+    (SELECT departmentId,MAX(salary) FROM Employee GROUP BY departmentId)
+'''
+
+#python(Pandas):
+'''
+
+'''
