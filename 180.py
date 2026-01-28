796ce0ff6c7b6084b71caadd26b05578d4d5184e
JENILKHUNT99 <jenilkhunt22222@gmail.com>
2026-01-28 21:51:37 +0530

    LeetCode 180

---
+++ 180.py
@@ -0,0 +1,16 @@
+#MySql:
+'''
+SELECT
+    DISTINCT L1.num AS ConsecutiveNums
+FROM Logs L1,Logs L2,Logs L3
+WHERE 
+    L1.num=L2.num AND
+    L2.num=L3.num AND
+    L1.id = L2.id-1 AND
+    L2.id = L3.id-1
+'''
+
+#python(Pandas):
+'''
+
+'''
