29e36faccfcee598c98416b7b4a6baed872285f5
JENILKHUNT99 <jenilkhunt22222@gmail.com>
2026-02-02 11:39:35 +0530

    LeetCode 619

---
+++ 619.py
@@ -0,0 +1,17 @@
+#MySQL:
+'''
+SELECT
+    MAX(num) AS num
+FROM MyNumbers 
+WHERE num IN (
+    SELECT 
+        num
+    FROM MyNumbers 
+    GROUP BY num
+    HAVING COUNT(*)=1
+)
+'''
+#python(Pandas):
+'''
+
+'''
