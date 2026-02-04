d8a2e1c55890ae19a669fac495fd292fd4a8daa9
JENILKHUNT99 <jenilkhunt22222@gmail.com>
2026-02-04 10:31:47 +0530

    LeetCode 602

---
+++ 602.py
@@ -0,0 +1,23 @@
+#MySQL:
+'''
+SELECT
+     id, SUM(num) AS num
+FROM (
+    SELECT
+        requester_id AS id, COUNT(requester_id) AS num
+    FROM RequestAccepted 
+    GROUP BY requester_id
+    UNION ALL
+    SELECT
+        accepter_id AS id , COUNT(accepter_id ) AS num
+    FROM RequestAccepted 
+    GROUP BY accepter_id
+) AS DataTable
+GROUP BY id
+ORDER BY num DESC
+LIMIT 1;
+'''
+#python(Pandas):
+'''
+
+'''
